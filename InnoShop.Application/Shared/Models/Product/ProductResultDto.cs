96b45107e45f2bac9fa6aab76105ab83a5fec2ef
filebin <alex.mousse9@gmail.com>
2024-02-14 02:00:50 +0300

    hotfix: json

--- InnoShop.Application/Shared/Models/Product/ProductResultDto.cs
+++ InnoShop.Application/Shared/Models/Product/ProductResultDto.cs
@@ -1,9 +1,33 @@
+using System.Text.Json.Serialization;
 using InnoShop.Domain.Abstraction;
 using InnoShop.Domain.Enums;
 
 namespace InnoShop.Application.Shared.Models.Product;
 
 public class ProductResultDto : IProduct {
+
+    [JsonConstructor]
+    [Newtonsoft.Json.JsonConstructor]
+    public ProductResultDto(
+    Guid id,
+    string title,
+    decimal price,
+    string description,
+    AvailabilityStatus availability,
+    DateTime creationTimestamp,
+    DateTime lastUpdateTimestamp,
+    string userId,
+    bool isEditable) {
+        Id = id;
+        UserId = userId;
+        Title = title;
+        Description = description;
+        Price = price;
+        Availability = availability;
+        CreationTimestamp = creationTimestamp;
+        LastUpdateTimestamp = lastUpdateTimestamp;
+        IsEditable = isEditable;
+    }
 
     public ProductResultDto(IProduct other) {
         Id = other.Id;
@@ -22,7 +46,7 @@ public class ProductResultDto : IProduct {
     public decimal Price { get; set; }
     public string Description { get; set; }
     public AvailabilityStatus Availability { get; set; }
-    
+
     public DateTime CreationTimestamp { get; set; }
     public DateTime LastUpdateTimestamp { get; set; }
 
