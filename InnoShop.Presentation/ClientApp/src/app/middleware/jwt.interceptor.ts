c0f1d890d73544e78ad5f7bb96735038a44002c0
filebin <alex.mousse9@gmail.com>
2024-02-14 14:51:34 +0300

    fixed frontend refresh logic

--- InnoShop.Presentation/ClientApp/src/app/middleware/jwt.interceptor.ts
+++ InnoShop.Presentation/ClientApp/src/app/middleware/jwt.interceptor.ts
@@ -9,36 +9,35 @@ import { Observable } from 'rxjs';
 import { AuthStateProviderService } from '../services/auth-state-provider.service';
 
 import { jwtDecode } from 'jwt-decode';
+import { UserApiProxyService } from '../services/user-api-proxy.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor(private authStateProvider: AuthStateProviderService) { }
+  constructor(private authStateProvider: AuthStateProviderService,
+    private userApi: UserApiProxyService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let access_token = this.authStateProvider.access_token;
 
-    if (access_token) {
-
+    if (access_token && !request.headers.has('Authorization')) {
       try {
         let exp = jwtDecode(access_token).exp;
         if (exp) {
-          if (exp * 1000 < (new Date().getTime() + 60_000)) {
-
+          if (exp * 1000 < (new Date().getTime() + 300_000)) {
+            this.userApi.refreshToken()
+              .subscribe({ error: err => { console.log(`refreshing failed\n err was ${err}`); } });
           }
         }
 
       } catch (err: any) {
         console.log(`parsing token failed\n err was ${err}`);
       }
-
-      if (!request.headers.has('Authorization')) {
-        request = request.clone({
-          setHeaders: {
-            Authorization: `Bearer ${access_token}`
-          }
-        });
-      }
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${access_token}`
+        }
+      });
     }
     return next.handle(request);
   }
